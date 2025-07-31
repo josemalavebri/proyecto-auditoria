397c45386c538de5f62e32a203ff38c5266cfffe
josemalavebri <jose.malavebri@gmail.com>
2025-07-31 09:26:57 -0500

    cambios

--- src/app/componentes/formularios/preguntas-formulario/preguntas-formulario.component.ts
+++ src/app/componentes/formularios/preguntas-formulario/preguntas-formulario.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import {
-  Form,
   FormBuilder,
   FormGroup,
+  FormsModule,
   ReactiveFormsModule,
 } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-preguntas-formulario',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './preguntas-formulario.component.html',
   styleUrl: './preguntas-formulario.component.css',
 })
@@ -19,11 +19,36 @@ export class PreguntasFormularioComponent {
   constructor(private fb: FormBuilder, private router: Router) {
     this.formulario = this.fb.group({
       resp_1: ['no'],
-      resp_2: [''],
+      resp_2: ['no'],
+      resp_3: ['no'],
+      resp_4: ['no'],
+      resp_5: ['no'],
+      resp_6: ['no'],
+      resp_7: ['no'],
+      resp_8: ['no'],
+      resp_9: ['no'],
+      resp_10: ['no'],
+      resp_11: ['no'],
     });
   }
 
   continuarCatalogo() {
     this.router.navigate(['/preguntas-v2']);
+  }
+  modalVisible = false;
+
+  porcentajeCumplimiento: number | null = null;
+
+  abrirModal() {
+    this.modalVisible = true;
+  }
+
+  cerrarModal() {
+    this.modalVisible = false;
+  }
+
+  aceptar() {
+    // lógica para guardar porcentaje
+    this.cerrarModal();
   }
 }
