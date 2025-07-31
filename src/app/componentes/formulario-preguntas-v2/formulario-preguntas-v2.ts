35bd31beb804e96a64a0196c5927de8d16aa586b
josemalavebri <jose.malavebri@gmail.com>
2025-07-30 21:51:26 -0500

    segundo borrador

--- src/app/componentes/formulario-preguntas-v2/formulario-preguntas-v2.ts
+++ src/app/componentes/formulario-preguntas-v2/formulario-preguntas-v2.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-preguntas-v2',
   templateUrl: './formulario-preguntas-v2.html',
   styleUrls: ['./formulario-preguntas-v2.css'],
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
 })
 export class FormularioPreguntasV2 {
   hoy: Date = new Date();
@@ -26,5 +27,35 @@ export class FormularioPreguntasV2 {
 
   finalizarFormulario() {
     this.router.navigate(['/resumen']);
+  }
+
+  modalVisible = false;
+  porcentajeCumplimiento: number | null = null;
+
+  abrirModal() {
+    this.modalVisible = true;
+    document.body.style.overflow = 'hidden';
+  }
+
+  cerrarModal() {
+    this.modalVisible = false;
+    document.body.style.overflow = '';
+  }
+
+  aceptar() {
+    if (
+      this.porcentajeCumplimiento === null ||
+      this.porcentajeCumplimiento < 0 ||
+      this.porcentajeCumplimiento > 100
+    ) {
+      alert('Ingrese un porcentaje válido entre 0 y 100');
+      return;
+    }
+    console.log('Porcentaje ingresado:', this.porcentajeCumplimiento);
+    // Aquí manejas el valor (guardar, enviar, etc)
+    this.cerrarModal();
+  }
+  abrirItem() {
+    this.router.navigate(['preguntas']);
   }
 }
